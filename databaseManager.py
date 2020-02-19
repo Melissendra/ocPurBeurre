6322b789066f4b99bef275240995094f3f76487f
Melissendra <melissendra77@gmail.com>
2020-02-19 18:58:49 +0100

    review of database manager

--- databaseManager.py
+++ databaseManager.py
@@ -12,13 +12,13 @@ class ProductsManager:
     def create_little_table(self, name_tab, name_id, name_col):
         """Method to create the database's small tables """
         self.db.query(
-            f'CREATE TABLE IF NOT EXISTS {name_tab}({name_id} SMALLINT NOT NULL AUTO_INCREMENT, {name_col} VARCHAR(50) NOT NULL UNIQUE, PRIMARY KEY({name_id}))'
+            f'CREATE TABLE IF NOT EXISTS {name_tab}({name_id} INT NOT NULL AUTO_INCREMENT, {name_col} VARCHAR(150) NOT NULL UNIQUE, PRIMARY KEY({name_id}))'
             )
 
     def create_big_table(self, name_tab, name_id, name_col1, name_col2, name_col3, name_col4, name_col5):
         """Method to create the database's big tables"""
-        self;db.query(
-            f'CREATE TABLE IF NOT EXISTS {name_tab}({name_id} SMALLINT NOT NULL AUTO_INCREMENT, {name_col1} VARCHAR(50) NOT NULL UNIQUE, {name_col2} VARCHAR(50) NOT NULL UNIQUE, {name_col3} CHAR(1) NOT NULL, {name_col4} VARCHAR(50) NOT NULL UNIQUE, {name_col5} VARCHAR(100) NOT NULL)'
+        self.db.query(
+            f'CREATE TABLE IF NOT EXISTS {name_tab}({name_id} BIGINT NOT NULL, {name_col1} VARCHAR(255) NOT NULL UNIQUE, {name_col2} VARCHAR(150) NOT NULL UNIQUE, {name_col3} CHAR(1) NOT NULL, {name_col4} VARCHAR(150) NOT NULL UNIQUE, {name_col5} VARCHAR(150) NOT NULL, PRIMARY KEY({name_id}))'
         ) 
 
     def drop_table(self, name_tab):
@@ -29,15 +29,27 @@ class ProductsManager:
 
     def insert_data(self, products, tab_name):
         """Method to insert datas in database's tables"""
-        api = client.ProductFetcher()
-        data = api.fetch_products()
-        products_cleaner = cleaner.Cleaner()
-        clean_products = products_cleaner.clean(data)
 
-        for product in clean_products['products']:
-            
+        for product in products:
+            product_data = product["products"]
+            product_id = product_data["id"]
+            cat_name = product_data["categories"]
+            product_name = product_data["brands"]
+            product_score = product_data["nutrition_score_fr"]
+            product_store = product_data["stores"]
+            product_link = product_data["url"]
+
+            self.db.query(f'INSERT INTO {tab_name}(product_id, cat_name) VALUES(:product_id, :cat_name)', product_id=product_id, cat_name=cat_name)
+
 
 if __name__ == '__main__':
+    api = client.ProductFetcher()
+    products = api.fetch_products()
+    cleaner = cleaner.Cleaner()
+    clean_products = cleaner.clean(products)
     manager = ProductsManager(db)
+
     manager.drop_table("categories")
-    manager.create_table("categories", "category_id", "category_name")
+    manager.create_little_table("categories", "category_id", "category_name")
+    manager.create_big_table("products", " product_id", "category_name", "product_brand", "nutriscore", "stores", "product_links")
+    manager.insert_data(clean_products, "categories")
